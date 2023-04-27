271d53aad7759fa068245a5aa4a523ace835b4e2
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-04-27 10:40:00 -0500

    proyecto

---
+++ proyecto_ejemplo/src/Producto.java
@@ -0,0 +1,25 @@
+
+
+
+public class Producto {
+    
+    String nombreP;
+    String id;
+    float precio;
+    
+    
+    public Producto(String nombreP, String id, float precio) {
+        this.nombreP = nombreP;
+        this.id = id;
+        this.precio = precio;
+     
+    }
+
+    
+
+    
+ 
+    
+    
+    
+}
